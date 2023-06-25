adea8fd92d26f496ab656053b114ad46b02be216
alex-toy <alexei.80@hotmail.fr>
2023-06-25 15:12:21 +0200

    filter users

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,15 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IUser } from '../models/user';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit{
   pageTitle : string = "user list";
-  users : any[] = [
+  filteredUsers : IUser[] = [];
+  users : IUser[] = [
     {id:1, name:"alex", email: 'alex@test.fr', phone:1234, country: 'lebanon'},
     {id:2, name:"seb", email: 'seb@test.fr', phone:9876, country: 'france'},
     {id:3, name:"kate", email: 'kate@test.fr', phone:4257, country: 'spain'},
   ];
+  showPhoneVisible : boolean = false;
+  private _listFilter: string = "";
+  public get listFilter(): string {
+    return this._listFilter;
+  }
+  public set listFilter(value: string) {
+    this._listFilter = value;
+    this.getFilteredUsers();
+  }
+
+  showPhone() : void{
+    this.showPhoneVisible = !this.showPhoneVisible;
+  }
+
+  getFilteredUsers() {
+    this.filteredUsers = this.users.filter(u => u.name.includes(this.listFilter));
+  }
+  
+  ngOnInit(): void {
+    console.log("ngOnInit")
+  }
 }
